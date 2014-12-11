4358430d176e18c538219101a690d9032610887f
Marc Leglise <mleglise@gmail.com>
2014-12-10 20:08:00 -0800

    Namespace admin routes. DRY

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
-Spree::Core::Engine.routes.draw do
-  get "admin/barcode/print_variants_barcodes/:id", to: "admin/barcode#print_variants_barcodes"
-  get "admin/barcode/print/:id", to: "admin/barcode#print"
+Spree::Core::Engine.add_routes do
+  namespace :admin do
+    get "barcode/print_variants_barcodes/:id", to: "barcode#print_variants_barcodes"
+    get "barcode/print/:id", to: "barcode#print"
 
-  get "admin/pos/new" , to: "admin/pos#new"
-  get "admin/pos/show/:number" , to: "admin/pos#show", as: :admin_pos_show_order
-  post "admin/pos/clean/:number" , to: "admin/pos#clean_order", as: :admin_pos_clean_order
-  get "admin/pos/find/:number" , to: "admin/pos#find"
-  get "admin/pos/add/:number/:item" , to: "admin/pos#add"
-  get "admin/pos/remove/:number/:item" , to: "admin/pos#remove"
-  post "admin/pos/associate_user/:number" , to: "admin/pos#associate_user"
-  post "admin/pos/update_payment/:number" , to: "admin/pos#update_payment"
-  post "admin/pos/update_line_item_quantity/:number" , to: "admin/pos#update_line_item_quantity" 
-  post "admin/pos/apply_discount/:number" , to: "admin/pos#apply_discount" 
+    get "pos/new", to: "pos#new", as: :new_pos_order
+    get "pos/show/:number" , to: "pos#show", as: :pos_show_order
+    post "pos/clean/:number" , to: "pos#clean_order", as: :pos_clean_order
+    get "pos/find/:number" , to: "pos#find"
+    get "pos/add/:number/:item" , to: "pos#add"
+    get "pos/remove/:number/:item" , to: "pos#remove"
+    post "pos/associate_user/:number" , to: "pos#associate_user"
+    post "pos/update_payment/:number" , to: "pos#update_payment"
+    post "pos/update_line_item_quantity/:number" , to: "pos#update_line_item_quantity"
+    post "pos/apply_discount/:number" , to: "pos#apply_discount" 
+    post "pos/update_stock_location/:number" , to: "pos#update_stock_location"
 
-  get "admin/pos/index" , to: "admin/pos#new"
-  post "admin/pos/update_stock_location/:number" , to: "admin/pos#update_stock_location"
-  get "admin/pos" , to: "admin/pos#new"
+    get "pos/index", to: "pos#new"
+    get "pos", to: "pos#new"
+  end
 end
