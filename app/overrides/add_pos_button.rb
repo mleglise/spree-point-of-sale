591ba1828f610092ca393f39b8c8eeb449b67d0a
Marc Leglise <mleglise@gmail.com>
2014-12-10 20:55:57 -0800

    Admin::Orders: update sidebar POS link to match Spree’s style.

--- app/overrides/add_pos_button.rb
+++ app/overrides/add_pos_button.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_order_submenu",
                      :name => "add_pos_button",
-                     :insert_after => ".sidebar",
+                     :insert_bottom => "[data-hook='admin_order_tabs']",
                      :partial => "spree/admin/orders/pos_button",
                      :disabled => false)
