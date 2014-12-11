f0be095037f81e7d1e30789052c67f6df287abfc
Marc Leglise <mleglise@gmail.com>
2014-12-10 20:09:38 -0800

    JavaScript: only execute when #pos-tabs is found on the page.
    Simple hack to make sure spree_pos.js doesn’t needlessly run on every backend page.

--- app/assets/javascripts/admin/spree_pos.js
+++ app/assets/javascripts/admin/spree_pos.js
@@ -1,20 +1,22 @@
 $( document ).ready(function() {
-  $('#sku').focus();
-  $( "#pos-tabs" ).tabs();
+  if( $('#pos-tabs').length > 0 ){
+    $('#sku').focus();
+    $( "#pos-tabs" ).tabs();
 
-  $( "#associate_user_toggle" ).click(function() {
-    $( "#associate_user" ).slideToggle( "slow", function() {
+    $( "#associate_user_toggle" ).click(function() {
+      $( "#associate_user" ).slideToggle( "slow", function() {
+      });
     });
-  });
 
-  $('#update_payment input[type=radio]').click(function() {
-    $('#card_name_options').val('');
-    $('#card_name_options').hide();
-  });
+    $('#update_payment input[type=radio]').click(function() {
+      $('#card_name_options').val('');
+      $('#card_name_options').hide();
+    });
 
-  $('#update_payment input[type=radio][data-name ~= card],#update_payment input[type=radio][data-name ~= Card]').click(function() {
-    $('#card_name_options').show();
-  });
-  $("#update_payment input[name='payment_method_id']:first").click();
-  $( "#associate_user" ).toggle();
+    $('#update_payment input[type=radio][data-name ~= card],#update_payment input[type=radio][data-name ~= Card]').click(function() {
+      $('#card_name_options').show();
+    });
+    $("#update_payment input[name='payment_method_id']:first").click();
+    $( "#associate_user" ).toggle();
+  }
 });
