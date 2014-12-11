177e5ff9342ce1ebfe17d730582c5099c2f305c1
Marc Leglise <mleglise@gmail.com>
2014-12-10 20:04:53 -0800

    Admin Menu: add calculator icon to POS tab.

--- app/overrides/pos_tab.rb
+++ app/overrides/pos_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "Add Pos tab to menu",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => " <%= tab( :pos , :url => admin_pos_path) %>",
+                     :text => " <%= tab(:pos, :url => admin_pos_path, :icon => 'calculator') %>",
                      :sequence => {:after => "promo_admin_tabs"},
                      :disabled => false)
